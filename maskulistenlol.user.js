78a31eea1223ad9153494f3b10b800927b983b16
lokubs <hallo@lnrt.de>
2017-10-17 23:47:03 +0200

    Create maskulistenlol.user.js

---
+++ maskulistenlol.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        maskulistenlol
+// @match       <all_urls>
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// ==/UserScript==
+
+$( "p:contains('Genderisierung')" ).html("Mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi.").css({'color': 'red'});
+
+$( "p:contains('Genderismus')" ).html("Mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi mimimi.").css({'color': 'red'});
